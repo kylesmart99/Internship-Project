525b7a1d1e558496c14f0c5f72f9aec6658a4c15
kylesmart99 <kylesmart99@gmail.com>
2020-07-10 15:36:23 -0600

    Added button styling to the buttons.

--- The Project/InternshipProject/src/app/app.module.ts
+++ The Project/InternshipProject/src/app/app.module.ts
@@ -4,20 +4,25 @@ import { ButtonPressingComponent } from './button-pressing/button-pressing.compo
 import { AppComponent } from './app.component';
 import { PasswordInputComponent } from './password-input/password-input.component';
 import { RouterModule } from '@angular/router';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PasswordInputComponent
+    PasswordInputComponent,
+    ButtonPressingComponent
   ],
   imports: [
   BrowserModule,
-    RouterModule.forRoot([
-      { path: 'home', component: PasswordInputComponent },
-      { path: 'buttons', component: ButtonPressingComponent },
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: '**', redirectTo: 'home', pathMatch: 'full' }
-    ])
+  //MatButtonModule,
+  RouterModule.forRoot([
+    { path: 'home', component: PasswordInputComponent },
+    { path: 'buttons', component: ButtonPressingComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  ]),
+  //BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
